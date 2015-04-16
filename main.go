221f401e6b6dfab144dff025b523e6c8a8965cbb
Abdulkadir Yaman <abdulkadiryaman@gmail.com>
2015-04-16 23:36:42 +0300

    disabling port opening tcp functions for heroku

--- main.go
+++ main.go
@@ -6,20 +6,19 @@ import (
 	"time"
 
 	"github.com/abdulkadiryaman/go-hamms/http"
-	"github.com/abdulkadiryaman/go-hamms/tcp"
 )
 
 func main() {
 	fmt.Println("Running Go hamms...")
 	port := os.Getenv("PORT")
 
-	go tcp.ListenAndDoNotAnswer()
-	go tcp.ListenAndAnswerWithEmptyString()
-	go tcp.ListenAndAnswerWithEmptyStringAfterClientSendsData()
-	go tcp.ListenAndAnswerWithMalformedStringImmediately()
-	go tcp.ListenAndAnswerWithMalformedStringAfterClientSendsData()
-	go tcp.ListenAndAnswerEvery5Seconds()
-	go tcp.ListenAndAnswerEvery30Seconds()
+	//go tcp.ListenAndDoNotAnswer()
+	//go tcp.ListenAndAnswerWithEmptyString()
+	//go tcp.ListenAndAnswerWithEmptyStringAfterClientSendsData()
+	//go tcp.ListenAndAnswerWithMalformedStringImmediately()
+	//go tcp.ListenAndAnswerWithMalformedStringAfterClientSendsData()
+	//go tcp.ListenAndAnswerEvery5Seconds()
+	//go tcp.ListenAndAnswerEvery30Seconds()
 	go http.StartRouter(port)
 
 	for {
